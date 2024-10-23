29bcd1841dbb4793f9c44059c08fb7f8b7d4bfb2
Bestian Tang <bestian@gmail.com>
2024-10-23 08:36:23 +0800

    init firebase

---
+++ src/firebase.ts
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { ref, getDatabase } from 'firebase/database'
+import { getAuth } from "firebase/auth"; 
+
+
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDU95egjb7rapaUJuVJYu0ZeSbP0OLaeCE",
+  authDomain: "autolearn-members.firebaseapp.com",
+  projectId: "autolearn-members",
+  storageBucket: "autolearn-members.appspot.com",
+  messagingSenderId: "535565819374",
+  appId: "1:535565819374:web:ccfe472f513439f3a002ff",
+  measurementId: "G-NTHZSSX71G",
+  // Add the correct database URL
+  databaseURL: "https://autolearn-members-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default app;
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export const projectsRef = ref(database, 'projects')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -25,24 +25,27 @@ div
 </template>
 
 <script lang="ts">
-import axios from 'axios'
+import { onMounted, ref } from 'vue'
+import { database, projectsRef } from '@/firebase'
+import { onValue } from 'firebase/database'
 
 export default {
-  data() {
+  setup() {
+    const data = ref([])
+
+    onMounted(() => {
+      onValue(projectsRef, (snapshot) => {
+        const projects = snapshot.val()
+        data.value = Object.values(projects)
+      }, (error) => {
+        console.error('讀取資料時出錯', error)
+      })
+    })
+
     return {
-      data: [],
+      data
     }
-  },
-  mounted() {
-    // 在組件掛載時發送請求
-    axios.get('https://members-backend.alearn13994229.workers.dev/api/projects')
-      .then(response => {
-        this.data = response.data
-      })
-      .catch(error => {
-        console.error('出錯了', error)
-      })
-  },
+  }
 }
 </script>
 
