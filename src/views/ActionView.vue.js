c0df63c3213c5b783824bc0f97987fac50bfdb81
Bestian Tang <bestian@gmail.com>
2024-12-19 10:10:40 +0800

    build

--- src/views/ActionView.vue.js
+++ src/views/ActionView.vue.js
@@ -1,6 +1,7 @@
 import { ref, onMounted, computed } from 'vue';
 import { actionsRef } from '@/firebase';
 import { push, onValue } from 'firebase/database';
+import { legislators } from '@/data/ly_11_02';
 export default (await import('vue')).defineComponent({
     setup() {
         const url = 'https://www.alearn.org.tw/action';
@@ -15,102 +16,6 @@ export default (await import('vue')).defineComponent({
         const shareToLine = () => {
             window.open(`https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(url)}`);
         };
-        const legislators = [
-            {
-                name: '范雲(召集委員)',
-                party: '民進黨',
-                email: 'vote4fanyun@gmail.com',
-                tel: '02-2358-6056',
-                fax: '02-2358-6060'
-            },
-            {
-                name: '萬美玲(召集委員)',
-                party: '國民黨',
-                tel: '02-2358-6156',
-                fax: '02-2358-6160'
-            },
-            {
-                name: '吳沛憶',
-                party: '民進黨',
-                tel: '02-2358-6166',
-                fax: '02-2358-6170',
-                email: 'tbslyc@gmail.com'
-            },
-            {
-                name: '吳春城',
-                party: '台灣民眾黨',
-                tel: '02-2358-8116',
-                fax: '02-2358-8120',
-                email: 'askme@brain.com.tw'
-            },
-            {
-                name: '林宜瑾',
-                party: '民進黨',
-                tel: '02-2358-8316',
-                fax: '02-2358-8320',
-                email: 'tbslyc@gmail.com'
-            },
-            {
-                name: '林倩綺',
-                party: '國民黨',
-                tel: '02-2358-6377',
-                fax: '02-2358-6380'
-            },
-            {
-                name: '柯志恩',
-                party: '國民黨',
-                tel: '02-2358-6876',
-                fax: '02-2358-6880'
-            },
-            {
-                name: '洪孟楷',
-                party: '國民黨',
-                tel: '02-2358-6811',
-                fax: '02-2358-6815'
-            },
-            {
-                name: '張雅琳',
-                party: '民進黨',
-                tel: '02-2358-8111',
-                fax: '02-2358-8115'
-            },
-            {
-                name: '郭昱晴',
-                party: '民進黨',
-                tel: '02-2358-6686',
-                fax: '02-2358-6690'
-            },
-            {
-                name: '陳秀寳',
-                party: '民進黨',
-                tel: '02-2358-6881',
-                fax: '02-2358-6140'
-            },
-            {
-                name: '陳培瑜',
-                party: '民進黨',
-                tel: '02-2358-8221',
-                fax: '02-2358-8225'
-            },
-            {
-                name: '葉元之',
-                party: '國民黨',
-                tel: '02-2358-6766',
-                fax: '02-2358-6905'
-            },
-            {
-                name: '葛如鈞',
-                party: '國民黨',
-                tel: '02-2358-6126',
-                fax: '02-2358-6130'
-            },
-            {
-                name: '羅廷瑋',
-                party: '國民黨',
-                tel: '02-2358-6116',
-                fax: '02-2358-6355'
-            }
-        ];
         const actions = ref([]);
         // 新增 computed property 來計算今日行動
         const todayActions = computed(() => {
