07b99515efae0ff927446bbe3911e59745b245cb
Bestian Tang <bestian@gmail.com>
2025-01-06 08:57:51 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { app } from '../firebase'
 import { getAuth } from 'firebase/auth'
 
 const router = createRouter({
