55747508a4982c200b597c9be1a5829e928f0e41
Bestian Tang <bestian@gmail.com>
2025-02-06 10:07:11 +0800

    work on #67 before build

--- src/views/DonateView.vue
+++ src/views/DonateView.vue
@@ -128,8 +128,9 @@ export default {
       .then(response => response.json())
       .then(data => {
         if (data.orderId) {
-          window.open('https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5', '_blank'); // 開啟綠界金流頁面 - 測試版
-          this.listenToOrderStatus(data.orderId);
+          // 跳轉到綠界金流頁面 - 測試版
+          window.location.href = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5';
+          // this.listenToOrderStatus(data.orderId);
         } else {
           alert('捐贈失敗，請改為其他方式捐贈');
         }
