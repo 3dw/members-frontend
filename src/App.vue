02c8418c970a22b692ae01d903d507a4ac5d9702
Bestian Tang <bestian@gmail.com>
2025-02-06 10:02:38 +0800

    在build測試前先藏住顯示層上的router-link以免被誤觸

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ header
       i.help.icon
       span 常見
       | 問題
-    RouterLink.item(v-if="devMode", to="/donate")
+    // RouterLink.item(v-if="devMode", to="/donate")
       i.money.icon
       | 捐贈
     RouterLink.item(v-if="!uid && !devMode", to="/history")
@@ -62,7 +62,7 @@ header
   RouterLink.item(to='/about', name="about")
     i.info.icon
     | 關於我們
-  RouterLink.item(to='/donate', name="donate")
+  // RouterLink.item(to='/donate', name="donate")
     i.money.icon
     | 捐贈本會
   RouterLink.item(to='/podcast', name="podcast")
@@ -176,7 +176,7 @@ export default defineComponent({
       photoURL: '',
       email: '',
       emailVerified: false,
-      devMode: true,
+      devMode: false,
       zoom: 13,
       center: [23.5330, 121.0654]
     }
