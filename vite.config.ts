2f5436c019dcefd378b46665c2b92ca936fed44a
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-07-30 13:43:24 +0800

    修改設定，讓build時不會生成.js和多餘的.js.map檔

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
-import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    nightwatchPlugin(),
-    checker({
-      typescript: true,
-    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   }
 })
